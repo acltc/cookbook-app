ef46036eca65818dfc98570528a8a5b552314aa1
Jay Wengrow <jaywngrw@gmail.com>
2014-06-22 15:24:14 -0500

    completed all seven RESTful actions for recipe resource

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -17,4 +17,18 @@ class RecipesController < ApplicationController
     # params[:recipe] = {"title"=>"Water", "author"=>"Jay", "ingredients"=>"Water", "directions"=>"Get water", "prep_time"=>"1 minute"}
     Recipe.create(params[:recipe])
   end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(params[:recipe])
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get '/recipes' => 'recipes#index'
   get '/recipes/new' => 'recipes#new'
   post '/recipes' => 'recipes#create'
-  get '/recipes/:id' => 'recipes#show'
+  get '/recipes/:id' => 'recipes#show', as: :recipe
+  get '/recipes/:id/edit' => 'recipes#edit'
+  patch '/recipes/:id' => 'recipes#update'
+  delete '/recipes/:id' => 'recipes#destroy'
+
 
 
   
